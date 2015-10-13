2149a18b7d428c0cfcec3f862c6b57892ac38487
Iqbal Ansari <iqbalansari02@yahoo.com>
2015-10-13 09:08:10 +0530

    Skeleton

---
+++ mu4e-alert.el
@@ -0,0 +1,34 @@
+;;; mu4e-alert.el --- Desktop notification and modeline for mu4e  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2015  Iqbal Ansari
+
+;; Author: Iqbal Ansari <iqbalansari02@yahoo.com>
+;; Keywords: mail, convenience
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; This package provides desktop notifications for mu4e, additionally it can
+;; display the number of unread emails in the modeline
+
+;;; Code:
+
+(require 'mu4e)
+(require 'alert)
+
+;; Do something
+
+(provide 'mu4e-alert)
+;;; mu4e-alert.el ends here
