710970bf42df558e3128ab7874b7c8350b4e6b52
Iqbal Ansari <iqbalansari02@yahoo.com>
2015-10-15 23:44:09 +0530

    Add package headers

--- mu4e-alert.el
+++ mu4e-alert.el
@@ -1,9 +1,12 @@
-;;; mu4e-alert.el --- Desktop notification and modeline for mu4e  -*- lexical-binding: t; -*-
+;;; mu4e-alert.el --- Desktop notification and mode-line display for mu4e  -*- lexical-binding: t; -*-
 
 ;; Copyright (C) 2015  Iqbal Ansari
 
 ;; Author: Iqbal Ansari <iqbalansari02@yahoo.com>
+;; URL: https://github.com/iqbalansari/mu4e-alert
 ;; Keywords: mail, convenience
+;; Version: 0.1
+;; Package-Requires: ((alert "1.2"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
